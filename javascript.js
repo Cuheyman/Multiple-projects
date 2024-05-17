d885027da0a2119e73b029e66e4e2d9e9d2c4a17
Cuheyman <cuheymankaya@live.dk>
2024-05-17 22:06:34 +0200

    Finished the Application

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function lowercase (string) {
+
+    return string.toLowerCase()
+}
+
+
+console.log(lowercase("HEJ"))
+
+
+forEach (taskData) {
+    const {id, title,date, description} = ""
+    
+  }
+
+
+taskData.forEach(task) {
+
+}
+
+
+
+function reset ( ) =>{
+
+}  
